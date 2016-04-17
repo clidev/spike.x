515a617f9e588023e2b06e8f0809d853c2272fd7
cli <clindevall@gmail.com>
2016-04-17 20:23:51 +0300

    Added exception handling around HAL getFileStores

--- spikex-metrics/src/main/java/io/spikex/metrics/Activator.java
+++ spikex-metrics/src/main/java/io/spikex/metrics/Activator.java
@@ -49,15 +49,15 @@ import oshi.util.FormatUtil;
  * @author cli
  */
 public final class Activator extends AbstractActivator {
-
+    
     private OshiMetrics m_oshiMetrics;
     private OSMXBeanMetrics m_osMXBeanMetrics;
     private MetricRegistry m_registry;
     private Map<String, Object> m_sharedData;
-
+    
     @Override
     protected void startVerticle() {
-
+        
         m_sharedData = vertx.sharedData().getMap(SHARED_METRICS_KEY);
 
         // OSHI system information
@@ -68,20 +68,20 @@ public final class Activator extends AbstractActivator {
         // Java OS MXBean information
         m_osMXBeanMetrics = new OSMXBeanMetrics();
         m_osMXBeanMetrics.init();
-        
+
         // Register metrics
         m_registry = new MetricRegistry();
-
+        
         logger().debug("Registering JVM garbage collection metric set");
         registerMetrics("jvm.gc", new GarbageCollectorMetricSet(), m_registry);
-
+        
         logger().debug("Registering JMV buffer metric set");
         registerMetrics("jvm.buffers", new BufferPoolMetricSet(
                 ManagementFactory.getPlatformMBeanServer()), m_registry);
-
+        
         logger().debug("Registering JMV memory usage metric set");
         registerMetrics("jvm.memory", new MemoryUsageGaugeSet(), m_registry);
-
+        
         logger().debug("Registering JMV thread states metric set");
         registerMetrics("jvm.threads", new ThreadStatesGaugeSet(), m_registry);
 
@@ -91,7 +91,7 @@ public final class Activator extends AbstractActivator {
         // Output system information
         outputSysInfo(systemInfo);
     }
-
+    
     @Override
     protected void handleTimerEvent() {
         //
@@ -103,12 +103,12 @@ public final class Activator extends AbstractActivator {
         m_oshiMetrics.publish(sharedData);
         m_osMXBeanMetrics.publish(sharedData);
     }
-
+    
     private void registerMetrics(
             final String prefix,
             final MetricSet metricSet,
             final MetricRegistry registry) {
-
+        
         for (Map.Entry<String, Metric> entry : metricSet.getMetrics().entrySet()) {
             if (entry.getValue() instanceof MetricSet) {
                 registerMetrics(prefix + "." + entry.getKey(), (MetricSet) entry.getValue(), registry);
@@ -117,7 +117,7 @@ public final class Activator extends AbstractActivator {
             }
         }
     }
-
+    
     private void registerAggregateMetrics(final MetricRegistry registry) {
         //
         // JVM used percentage
@@ -125,9 +125,9 @@ public final class Activator extends AbstractActivator {
         Map<String, Gauge> gauges = registry.getGauges();
         final Gauge<Long> gaugeJvmMemMax = gauges.get("jvm.memory.total.max");
         final Gauge<Long> gaugeJvmMemUsed = gauges.get("jvm.memory.total.used");
-
+        
         registry.register(JVM_MEM_PERC.key(), new CachedGauge<Double>(5L, TimeUnit.SECONDS) {
-
+            
             @Override
             protected Double loadValue() {
                 double memMax = gaugeJvmMemMax.getValue();
@@ -136,13 +136,13 @@ public final class Activator extends AbstractActivator {
             }
         });
     }
-
+    
     private void outputSysInfo(final SystemInfo sysInfo) {
-
+        
         logger().info("======================================================");
         logger().info("{}", sysInfo.getOperatingSystem());
         logger().info("------------------------------------------------------");
-
+        
         HardwareAbstractionLayer hal = sysInfo.getHardware();
         Processor[] cpus = hal.getProcessors();
         logger().info("CPU count: {}", cpus.length);
@@ -150,36 +150,41 @@ public final class Activator extends AbstractActivator {
         for (Processor cpu : cpus) {
             logger().info("{}: {}", n++, cpu);
         }
-
+        
         long memTotal = hal.getMemory().getTotal();
         long memAvail = hal.getMemory().getAvailable();
         long memUsed = memTotal - memAvail;
-
+        
         logger().info("Used memory: {}", FormatUtil.formatBytes(memUsed));
         logger().info("Available memory: {}", FormatUtil.formatBytes(memAvail));
         logger().info("Total memory: {}", FormatUtil.formatBytes(memTotal));
 
-        for (OSFileStore fs : hal.getFileStores()) {
-            logger().info("{} ({}) Used: {} Free: {} Total: {}",
-                    fs.getName(),
-                    fs.getDescription(),
-                    FormatUtil.formatBytes(fs.getTotalSpace() - fs.getUsableSpace()),
-                    FormatUtil.formatBytes(fs.getUsableSpace()),
-                    FormatUtil.formatBytes(fs.getTotalSpace()));
+        // This can explode in a NullPointerException (at sun.awt.shell.Win32ShellFolder2.access$200(Win32ShellFolder2.java:72))
+        try {
+            for (OSFileStore fs : hal.getFileStores()) {
+                logger().info("{} ({}) Used: {} Free: {} Total: {}",
+                        fs.getName(),
+                        fs.getDescription(),
+                        FormatUtil.formatBytes(fs.getTotalSpace() - fs.getUsableSpace()),
+                        FormatUtil.formatBytes(fs.getUsableSpace()),
+                        FormatUtil.formatBytes(fs.getTotalSpace()));
+            }
+        } catch (Exception e) {
+            logger().error("Failed to retrieve file store information", e);
         }
-
+        
         logger().info("======================================================");
     }
-
+    
     private void publishGauges(final Map<String, Object> sharedData) {
-
+        
         Map<String, Gauge> gauges = m_registry.getGauges();
         for (Map.Entry<String, Gauge> entry : gauges.entrySet()) {
-
+            
             String key = entry.getKey();
             Gauge gauge = entry.getValue();
             Object value = gauge.getValue();
-
+            
             if (value instanceof Set) {
                 StringBuilder sb = new StringBuilder();
                 Set<Object> values = (Set) value;
@@ -194,31 +199,31 @@ public final class Activator extends AbstractActivator {
                     value = sb.toString();
                 }
             }
-
+            
             logger().trace("{} = {} ({})",
                     key,
                     value,
                     value.getClass().getName());
-
+            
             sharedData.put(key, value); // Actual value
             sharedData.put(key + METRIC_DSTYPE, TYPE_GAUGE); // Datasource type (GAUGE, COUNTER, etc..)
         }
     }
-
+    
     private void publishCounters(final Map<String, Object> sharedData) {
-
+        
         Map<String, Counter> counters = m_registry.getCounters();
         for (Map.Entry<String, Counter> entry : counters.entrySet()) {
-
+            
             String key = entry.getKey();
             Counter counter = entry.getValue();
             long value = counter.getCount();
-
+            
             logger().trace("{} = {} ({})",
                     key,
                     value,
                     Long.class.getName());
-
+            
             sharedData.put(key, value); // Actual value
             sharedData.put(key + METRIC_DSTYPE, TYPE_COUNTER); // Datasource type (GAUGE, COUNTER, etc..)
         }
